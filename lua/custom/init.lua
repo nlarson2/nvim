3a2eb0fa81b0f382f3ff0fabf3751bca8bbb7da8
nick <nlarson2@user.noreply.github.com>
2023-07-11 08:55:21 -0500

    Added keymap for neotree

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -29,3 +29,7 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 --vim.opt.colorcolumn = "80"
+
+-- keymap to open :NeotreeToggle
+vim.api.nvim_set_keymap("n", "<C-n>", ":Neotree<CR>", { noremap = true, silent = true })
+
