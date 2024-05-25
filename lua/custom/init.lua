2230cb9906b4446c46703c09ffe22f3504f6b2ca
nick larson <28692007+nlarson2@users.noreply.github.com>
2024-05-25 16:40:13 +0000

    harpoon setup

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,3 +1,5 @@
+local harpoon = require("harpoon")
+
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
@@ -33,3 +35,14 @@ vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
 -- keymap to open :NeotreeToggle
 vim.api.nvim_set_keymap("n", "<C-n>", ":Neotree<CR>", { noremap = true, silent = true })
 
+
+-- harpoon setup
+harpoon:setup()
+
+vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>h;", function() harpoon:list():select(4) end)
